4469ec82d34d2000d52d2188e836cfe4d4c4a066
wushuo <2725730722@qq.com>
2023-01-27 19:49:23 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,78 @@
+<template>
+  <div id="main">
+    <div style="display: flex;">
+      <n-upload
+          multiple
+          directory-dnd
+          v-model:file-list="files"
+      >
+        <n-upload-dragger>
+          <div style="margin-bottom: 12px">
+            <n-icon size="48" :depth="3">
+              <archive-icon/>
+            </n-icon>
+          </div>
+          <n-text style="font-size: 16px">
+            点击或者拖动文件到该区域
+          </n-text>
+        </n-upload-dragger>
+      </n-upload>
+      <n-button tertiary circle style="margin-left: 5px;" @click="files=[]">
+        <template #icon>
+          <n-icon>
+            <Clean/>
+          </n-icon>
+        </template>
+      </n-button>
+    </div>
+    <n-select v-model:value="selType" :options="Object.keys(types).map(v =>  {
+      return {
+        label:v,
+        value:v
+      }
+    })"></n-select>
+    <div style="display: flex;flex-flow: row-reverse;margin-top: 5px;">
+      <n-button @click="cover" :disabled="files.length < 1">转换</n-button>
+    </div>
+  </div>
+</template>
+<script setup>
+import {ArchiveOutline as ArchiveIcon} from "@vicons/ionicons5";
+import {ref} from "vue";
+import {Clean} from "@vicons/carbon";
+
+
+let files = ref([])
+let selType = ref('7z')
+let types = ref({
+  '7z': [55, 122, 188, 175],
+  'rar': [83, 97, 114, 33],
+  'zip': [80, 75, 3, 4],
+  'exe': [77, 90, 96, 0],
+  'iso': [51, 237, 144, 144],
+  'apk': [80, 75, 3, 4]
+})
+
+let cover = () => {
+  for (let file of files.value) {
+    let fileReader = new FileReader();
+    fileReader.onloadend = (v) => {
+      let uint8Array = new Uint8Array(v.target.result);
+      let type = types.value[selType.value];
+      type.forEach((v, i) => {
+        uint8Array[i] = v;
+      })
+      let url = window.URL.createObjectURL(new Blob([uint8Array.buffer], {type: "arraybuffer"}))
+      const link = document.createElement('a');
+      link.style.display = 'none';
+      link.href = url;
+      link.setAttribute('download', file.name + '.' + selType.value);
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    }
+    fileReader.readAsArrayBuffer(file['file'])
+  }
+}
+
+</script>
